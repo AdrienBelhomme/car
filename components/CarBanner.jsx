e292303cc3f82322cf56779773a12b40799e1a06
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-10-31 23:59:20 -0400

    fix carrent home page banner

--- components/CarBanner.jsx
+++ components/CarBanner.jsx
@@ -7,15 +7,15 @@ import Button from './Button';
 const CarBanner = (props) => {
   const { title, subtitle, carImg, card, btnColor } = props;
   return (
-    <div className={`${card} w-640 h-360  pl-6 pt-6 card border rounded-xl `}>
-      <div className="justify-start w-284 h-208 flex flex-wrap flex-col">
-        <h1 className="font-jakarta text-white-color font-semibold text-3xl">{title}</h1>
-        <h2 className="font-jakarta text-white-color pt-4">{subtitle}</h2>
-        <div className="pt-5">
-          <Button text="Rental Car" bgColor={btnColor} />
+    <div className={` flex flex-col ${card} w-640 h-360  border rounded-xl`}>
+      <div className=" flex flex-col justify-start  pt-6 pl-6 max-w-xs ">
+        <h1 className=" font-jakarta text-white-color font-semibold text-3xl text-left">{title}</h1>
+        <h2 className=" font-jakarta text-white-color pt-4">{subtitle}</h2>
+        <div className="pt-7">
+          <Button text="Rental Car" bgColor={btnColor} onClick={() => {}} />
         </div>
       </div>
-      <div>
+      <div className="flex justify-center items-center mt-4 mb-4 flex-shrink-1">
         <Image src={carImg} width="406px" height="116px" alt="carimage" priority />
       </div>
     </div>
--- pages/car-rent.js
+++ pages/car-rent.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 import { CarBanner } from '../components';
 import image from '../assets/index';
 
 const CarRent = () => (
-  <div className="w-full h-screen mt-124 bg-bg-color-car-app">
-    <div className="flex gap-x-8 pt-8 px-16 ">
+  <div className=" container flex flex-col gap-8 md:justify-center px-4 pt-8 mx-auto space-y-12 md:space-y-0 md:flex-row h-screen mt-124 bg-bg-color-car-app">
+    <div className="item flex max-w-xl">
       <CarBanner
         title="The Best Platform for Car Rental"
         subtitle="Ease of doing a car rental safely and reliably. Of course at a low price."
@@ -13,18 +11,18 @@ const CarRent = () => (
         card="bg-[#5CAFFC]"
         btnColor="bg-btn-blue"
       />
+    </div>
+    <div className=" item hidden md:flex  max-w-xl ">
       <CarBanner
         title="Easy way to rent a car at a low price"
         subtitle="Providing cheap car rental services and safe and comfortable facilities."
         carImg={image.nissangtr}
         card="bg-btn-blue"
         btnColor="bg-[#5CAFFC]"
-
       />
     </div>
-
   </div>
-
 );
 
 export default CarRent;
+
