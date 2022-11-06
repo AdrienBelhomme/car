7da7d0a231797e3d5b2d75213e0fa9aa966e4792
C1okesta <c1okesdev@gmail.com>
2022-11-05 17:59:37 -0700

    adding to assets

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,17 @@
 import koenigsegg from './koenigsegg.png';
 import nissangtr from './nissangtr.png';
-import VuesaxBoldHeart1 from './VuesaxBoldHeart1';
-import VuesaxBoldNotification from './VuesaxBoldNotification';
-import VuesaxBoldSetting2 from './VuesaxBoldSetting2';
+import Like from './Like.png';
+import Notification from './Notification.png';
+import Profile from './Profile.png';
+import Settings from './Settings.png';
 
 
 export default {
   koenigsegg,
   nissangtr,
-  VuesaxBoldSetting2,
-  VuesaxBoldNotification,
-  VuesaxBoldHeart1,
-
+  Settings,
+  Profile,
+  Notification,
+  Like,
 
 };
