752ac922c3bc05f3346584035a705db12d66397c
Tidbits JS <tidbitsjs@gmail.com>
2022-11-06 20:03:39 +0530

    care data list restructure

--- components/CarTypeList.jsx
+++ components/CarTypeList.jsx
@@ -4,25 +4,45 @@ import React from 'react';
 import { CarCard } from './index';
 import image from '../assets/index';
 
-const CarTypeList = ({ CarData, scrollable, noscroll }) => (
+// const CarTypeList = ({ carData, scrollable, noscroll }) => (
+//   <div>
+//     {carData.map(({ title, type }) => (
+//       <div className="popular w-full mt-8 md:mt-[42px]">
+//         <div className="flex justify-between mx-5">
+//           <h1 className="flex text-secondinary-light-300 font-medium text-sm md:text-base md:font-semi-bold" key={title}>
+//             {title}
+//           </h1>
+//           <button type="button" onClick={() => {}}>
+//             <h1 className="flex justify-end text-btn-blue text-xs font-semibold md:text-base">view all</h1>
+//           </button>
+//         </div>
+//         <div className={`flex mt-[30px] justify-start w-full ${noscroll} gap-8 ${scrollable} `}>
+//           {type.map((model, index) => (
+//             <div key={index} className="min-w-[280px] flex-1"> <CarCard model={model} image={Object.values(image)[index]} /></div>
+//           ))}
+//         </div>
+//       </div>
+//     ))}
+//   </div>
+// );
+
+const CarTypeList = ({ carData, scrollable, noscroll }) => (
   <div>
-    {CarData.map(({ title, type }) => (
-      <div className="popular w-full mt-8 md:mt-[42px]">
-        <div className="flex justify-between mx-5">
-          <h1 className="flex text-secondinary-light-300 font-medium text-sm md:text-base md:font-semi-bold" key={title}>
-            {title}
-          </h1>
-          <button type="button" onClick={() => {}}>
-            <h1 className="flex justify-end text-btn-blue text-xs font-semibold md:text-base">view all</h1>
-          </button>
-        </div>
-        <div className={`flex mt-[30px] justify-start w-full ${noscroll} gap-8 ${scrollable} `}>
-          {type.map((model, index) => (
-            <div key={index} className="min-w-[280px] flex-1"> <CarCard model={model} image={Object.values(image)[index]} /></div>
-          ))}
-        </div>
+    <div className="popular w-full mt-8 md:mt-[42px]">
+      <div className="flex justify-between mx-5">
+        <h1 className="flex text-secondinary-light-300 font-medium text-sm md:text-base md:font-semi-bold">
+          {carData.type}
+        </h1>
+        <button type="button" onClick={() => {}}>
+          <h1 className="flex justify-end text-btn-blue text-xs font-semibold md:text-base">view all</h1>
+        </button>
       </div>
-    ))}
+      <div className={`flex mt-[30px] justify-start w-full ${noscroll} gap-8 ${scrollable} `}>
+        {carData.cars.map((car, index) => (
+          <div key={car.model + index} className="min-w-[280px] flex-1"> <CarCard {...car} /></div>
+        ))}
+      </div>
+    </div>
   </div>
 );
 
--- pages/car-rent.js
+++ pages/car-rent.js
@@ -1,50 +1,77 @@
 import { Button, CarBanner, StatePicker, CarTypeList } from '../components';
 import image from '../assets/index';
 
-const CarRent = () => {
-  const popular = [
-    {
-      title: 'Popular',
-      type: ['Koenigsegg', 'Nissan GT - R', 'Rolls-Royce', 'Nissan GT - R', 'Koenigsegg', 'Koenigsegg'],
-    }];
-  const recommended = [{
+const popular = [
+  {
+    title: 'Popular',
+    type: ['Koenigsegg', 'Nissan GT - R', 'Rolls-Royce', 'Nissan GT - R', 'Koenigsegg', 'Koenigsegg'],
+  },
+];
+
+const recommended = [
+  {
     title: 'Recommended Cars',
     type: ['Koenigsegg', 'Nissan GT - R', 'Rolls-Royce', 'All New Rush', 'All New Terio', 'CRV', 'New MGZS', 'Exclusive MGZS'],
   },
-  ];
+];
 
-  return (
-    <div className=" p-6 md:p-16">
-      <div className="flex flex-col gap-8 pt-8   md:flex-row  mt-124 bg-bg-color-car-app">
-        <div className=" flex w-full ">
-          <CarBanner
-            title="The Best Platform for Car Rental"
-            subtitle="Ease of doing a car rental safely and reliably. Of course at a low price."
-            carImg={image.koenigsegg}
-            card="bg-[#5CAFFC]"
-            btnColor="bg-btn-blue"
-          />
-        </div>
-        <div className=" hidden md:flex w-full ">
-          <CarBanner
-            title="Easy way to rent a car at a low price"
-            subtitle="Providing cheap car rental services and safe and comfortable facilities."
-            carImg={image.nissan}
-            card="bg-btn-blue"
-            btnColor="bg-[#5CAFFC]"
-          />
-        </div>
+// This is how your data format will be from backend.
+const popularNew = {
+  type: 'Popular',
+  cars: [
+    {
+      image: image.nissan,
+      model: 'Ferrari',
+      category: 'Sport',
+      gas: '90L',
+      type: 'Manual',
+      people: '2 people',
+      price: '99.00',
+    },
+    {
+      image: image.koenigsegg,
+      model: 'Ferrari',
+      category: 'Sport',
+      gas: '90L',
+      type: 'Manual',
+      people: '2 people',
+      price: '99.00',
+    },
+  ],
+};
+
+const CarRent = () => (
+  <div className=" p-6 md:p-16">
+    <div className="flex flex-col gap-8 pt-8   md:flex-row  mt-124 bg-bg-color-car-app">
+      <div className=" flex w-full ">
+        <CarBanner
+          title="The Best Platform for Car Rental"
+          subtitle="Ease of doing a car rental safely and reliably. Of course at a low price."
+          carImg={image.koenigsegg}
+          card="bg-[#5CAFFC]"
+          btnColor="bg-btn-blue"
+        />
       </div>
-      <div className="mt-[42px]">
-        <StatePicker />
-      </div>
-      <CarTypeList CarData={popular} scrollable="overflow-x-auto md:flex-wrap" />
-      <CarTypeList CarData={recommended} noscroll="flex-wrap" />
-      <div className="flex justify-center items-center mt-12 md:mt-16">
-        <Button text="Show more cars" bgColor="bg-btn-blue" color="text-white" onClick={() => {}} />
+      <div className=" hidden md:flex w-full ">
+        <CarBanner
+          title="Easy way to rent a car at a low price"
+          subtitle="Providing cheap car rental services and safe and comfortable facilities."
+          carImg={image.nissan}
+          card="bg-btn-blue"
+          btnColor="bg-[#5CAFFC]"
+        />
       </div>
     </div>
-  );
-};
+    <div className="mt-[42px]">
+      <StatePicker />
+    </div>
+    <CarTypeList carData={popularNew} scrollable="overflow-x-auto md:flex-wrap" />
+    {/* <CarTypeList carData={recommended} noscroll="flex-wrap" /> */}
+    <div className="flex justify-center items-center mt-12 md:mt-16">
+      <Button text="Show more cars" bgColor="bg-btn-blue" color="text-white" onClick={() => {}} />
+    </div>
+  </div>
+);
+
 export default CarRent;
 
