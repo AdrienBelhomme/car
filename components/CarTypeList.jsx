792d0faca13869380011ccbdf3bb2f7f0147de8d
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-11-07 10:05:18 -0400

    fix conflicts

--- components/CarTypeList.jsx
+++ components/CarTypeList.jsx
@@ -2,29 +2,6 @@
 import React from 'react';
 
 import { CarCard } from './index';
-// import image from '../assets/index';
-
-// const CarTypeList = ({ carData, scrollable, noscroll }) => (
-//   <div>
-//     {carData.map(({ title, type }) => (
-//       <div className="popular w-full mt-8 md:mt-[42px]">
-//         <div className="flex justify-between mx-5">
-//           <h1 className="flex text-secondinary-light-300 font-medium text-sm md:text-base md:font-semi-bold" key={title}>
-//             {title}
-//           </h1>
-//           <button type="button" onClick={() => {}}>
-//             <h1 className="flex justify-end text-btn-blue text-xs font-semibold md:text-base">view all</h1>
-//           </button>
-//         </div>
-//         <div className={`flex mt-[30px] justify-start w-full ${noscroll} gap-8 ${scrollable} `}>
-//           {type.map((model, index) => (
-//             <div key={index} className="min-w-[280px] flex-1"> <CarCard model={model} image={Object.values(image)[index]} /></div>
-//           ))}
-//         </div>
-//       </div>
-//     ))}
-//   </div>
-// );
 
 const CarTypeList = ({ carData, scrollable, noscroll }) => (
   <div>
@@ -48,6 +25,3 @@ const CarTypeList = ({ carData, scrollable, noscroll }) => (
 
 export default CarTypeList;
 
-// flex mt-[30px] justify-start w-full md:flex-wrap gap-8 overflow-x-auto
-
-// flex mt-[30px] justify-start w-full flex-wrap gap-8
